e62a1251f09f9483b9ad95393da2b2055155fd6a
kedeger <kedeger@uw.edu>
2016-08-01 12:54:05 -0700

    Bootstrp Microsoft Mockup, Incomplete (Kemal)

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+//set document ready
+$(document).ready(function() {
+    // $("#imgSlide").append('<div><img class="slide" src="images/microsoft.png"></div> ')
+
+    $('.single-item').slick({
+    });
+
+}
